a209b17bb7d87caaee104a03ee6f7ac68b4f8e16
Munyoung Hwang <munyoung@munyoung-laptop.(none)>
2009-09-22 01:20:09 +0900

    home_linux: remove line for selecting color theme

--- home_linux.el
+++ home_linux.el
@@ -16,7 +16,7 @@
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "Grey15" :foreground "Grey" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "bitstream" :family "Courier 10 Pitch")))))
 
-(color-theme-charcoal-black)
+;(color-theme-charcoal-black)
 (set-fontset-font "fontset-default" '(#x1100 . #xffdc)  '("NanumGothicCoding" . "unicode-bmp")) ;;; 유니코드 한글영역...Malgun Gothic에다가 원하는폰트를 적는다
 (set-fontset-font "fontset-default" '(#xe0bc . #xf66e)  '("NanumGothicCoding" . "unicode-bmp")) ;;;유니코드 사용자 영역
 (setq x-super-keysym 'meta)
