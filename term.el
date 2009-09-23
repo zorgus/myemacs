4b3665d7a1f80226a064701f950ff4bb1751d5d7
Munyoung Hwang <munyoung@lge.com>
2009-09-23 14:08:22 +0900

    term: add specific setting file for terminal

---
+++ term.el
@@ -0,0 +1,20 @@
+(color-theme-tty-dark)
+;(color-theme-comidia)
+(setq dired-recursive-deletes 'top)	;; dired - recursive delete directory
+(setenv "PAGER" "cat")
+(setq inhibit-splash-screen t)		;; disable splash screen
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (list "~/doc/todo.org")))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :family "default"))))
+ '(magit-item-highlight ((((class color) (background dark)) (:background "dim gray" :foreground "black"))))
+ '(magit-log-head-label ((((class color) (background dark)) (:background "blue")))))
