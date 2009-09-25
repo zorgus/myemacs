32fc92e49d402419109e88dd0b87a5060c8fb5a3
Munyoung Hwang <munyoung@lge.com>
2009-09-25 13:54:37 +0900

    frame_save: add package for saving frame and restroing it later

---
+++ packages/frame_save.el
@@ -0,0 +1,35 @@
+; util for saving current frame and restoring it later
+;
+
+(defparameter th-frame-config-register ?°
+  "The register which is used for storing and restoring frame
+configurations by `th-save-frame-configuration' and
+`th-jump-to-register'.")
+
+(defun th-save-frame-configuration (arg)
+  "Stores the current frame configuration in register
+`th-frame-config-register'. If a prefix argument is given, you
+can choose which register to use."
+  (interactive "P")
+  (let ((register (if arg
+                      (read-char "Which register? ")
+                    th-frame-config-register)))
+    (frame-configuration-to-register register)
+    (message "Frame configuration saved in register '%c'."
+             register)))
+
+(defun th-jump-to-register (arg)
+  "Jumps to register `th-frame-config-register'. If a prefix
+argument is given, you can choose which register to jump to."
+  (interactive "P")
+  (let ((register (if arg
+                      (read-char "Which register? ")
+                    th-frame-config-register)))
+    (jump-to-register register)
+    (message "Jumped to register '%c'."
+             register)))
+
+(global-set-key (kbd "<F5>")
+                'th-save-frame-configuration)
+(global-set-key (kbd "<F6>")
+                'th-jump-to-register)
