7776b5b6fe310c4cf00e42d2c874afb112ddcaa5
Munyoung Hwang <munyoung.hwang@lge.com>
2010-10-10 11:52:00 +0900

    Remove unused packages

--- emacs.el
+++ emacs.el
@@ -5,40 +5,40 @@
 ;; file: emacs.el
 ;; description: common settings for emacs
 
-(require 'cl)
+;; (require 'cl)
 
-(global-font-lock-mode 1)               ; syntanx highlight
-(transient-mark-mode t)                 ; marking highlight
-(show-paren-mode t)
-(if (functionp 'global-hi-lock-mode)
-    (global-hi-lock-mode 1)
-  (hi-lock-mode 1))
-;;(global-hl-line-mode 1)
-;(setq ring-bell-function (lambda () nil))
+;; (global-font-lock-mode 1)               ; syntanx highlight
+;; (transient-mark-mode t)                 ; marking highlight
+;; (show-paren-mode t)
+;; (if (functionp 'global-hi-lock-mode)
+;;     (global-hi-lock-mode 1)
+;;   (hi-lock-mode 1))
+;; ;;(global-hl-line-mode 1)
+;; ;(setq ring-bell-function (lambda () nil))
 
-(line-number-mode 1)
-(column-number-mode 1)
+;; (line-number-mode 1)
+;; (column-number-mode 1)
 
-(setq scroll-step 1)
-(setq scroll-conservatively 4096)
+;; (setq scroll-step 1)
+;; (setq scroll-conservatively 4096)
 
-(setq dired-recursive-deletes 'top)	;; dired - recursive delete directory
+;; (setq dired-recursive-deletes 'top)	;; dired - recursive delete directory
 (setq inhibit-splash-screen t)		;; disable splash screen
                                         
-(delete-selection-mode 1)
+;; (delete-selection-mode 1)
 
-;(setq-default truncate-lines t)
+;; ;(setq-default truncate-lines t)
 
-(dynamic-completion-mode)
+;; (dynamic-completion-mode)
 
-;; Set the text for titlebar and icons, %f=filename, %b=buffername
-(setq frame-title-format (list "GNU Emacs " emacs-version " - " '(buffer-file-name "%f" "%b")))
-(setq icon-title-format frame-title-format)
+;; ;; Set the text for titlebar and icons, %f=filename, %b=buffername
+;; (setq frame-title-format (list "GNU Emacs " emacs-version " - " '(buffer-file-name "%f" "%b")))
+;; (setq icon-title-format frame-title-format)
 
-(which-function-mode 1)
+;; (which-function-mode 1)
 
-;; (tool-bar-mode -1)
-;; (menu-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
 
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
@@ -63,38 +63,38 @@
 (setq indent-tabs-mode t) ;; no tab
 ))
 
-;; compilation window 10 height
-(setq compilation-window-height 10)
+;; ;; compilation window 10 height
+;; (setq compilation-window-height 10)
 
-;; eshell
-(global-set-key (kbd "M-0") 'eshell)
+;; ;; eshell
+;; (global-set-key (kbd "M-0") 'eshell)
 
-(setq eshell-save-history-on-exit t)
-;(add-hook 'eshell-mode-hook
-;          '(lambda () (define-key eshell-mode-map "\t" 'pcomplete-list)))
-;(setq eshell-cmpl-cycle-completions nil)
+;; (setq eshell-save-history-on-exit t)
+;; ;(add-hook 'eshell-mode-hook
+;; ;          '(lambda () (define-key eshell-mode-map "\t" 'pcomplete-list)))
+;; ;(setq eshell-cmpl-cycle-completions nil)
 
-;; org mode
-(setq org-log-done t)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; ;; org mode
+;; (setq org-log-done t)
+;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
 ;; transparency frame
-;; (when window-system
-;; (modify-frame-parameters nil '((alpha . 90))))
+(when window-system
+(modify-frame-parameters nil '((alpha . 80))))
 
-;; windmove
-(require 'windmove)
-(global-set-key (kbd "M-H") 'windmove-left)
-(global-set-key (kbd "M-J") 'windmove-down)
-(global-set-key (kbd "M-K") 'windmove-up)
-(global-set-key (kbd "M-L") 'windmove-right)
+;; ;; windmove
+;; (require 'windmove)
+;; (global-set-key (kbd "M-H") 'windmove-left)
+;; (global-set-key (kbd "M-J") 'windmove-down)
+;; (global-set-key (kbd "M-K") 'windmove-up)
+;; (global-set-key (kbd "M-L") 'windmove-right)
 
-;; fix problem that open file slowly
-(setq vc-handled-backends nil)
+;; ;; fix problem that open file slowly
+;; (setq vc-handled-backends nil)
 
-;; shortcut for M-x
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c\C-m" 'execute-extended-command)
+;; ;; shortcut for M-x
+;; (global-set-key "\C-x\C-m" 'execute-extended-command)
+;; (global-set-key "\C-c\C-m" 'execute-extended-command)
 
 ;; linum-mode (emacs 23 only)
 ;; (global-linum-mode 1)
@@ -141,7 +141,7 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; Control-tab
-(global-set-key (kbd "<C-tab>") 'bury-buffer)
+;; (global-set-key (kbd "<C-tab>") 'bury-buffer)
 
 ;; color theme
 (add-to-list 'load-path "~/emacs/packages/color-theme-6.6.0/")
@@ -156,7 +156,7 @@
 ;; (add-hook 'twitter-status-edit-mode-hook 'longlines-mode)
 
 ;; yasnippet
-(require 'yasnippet-bundle)
+;; (require 'yasnippet-bundle)
 
 ;; xcscope
 (require 'xcscope)
@@ -186,3 +186,9 @@
 ;; zenburn
 (require 'zenburn)
 (zenburn)
+
+(defun remove-dos-eol ()
+  "Removes the disturbing '^M' showing up in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
